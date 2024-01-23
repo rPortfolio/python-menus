65eee3ae46ee1477a3403bdd70c234eb2908d5d0
Romeo Cifuentes-Beach <rcifuentesbeach@ucvts.org>
2024-01-22 23:35:15 -0500

    Fix: Indentation

--- example.py
+++ example.py
@@ -6,41 +6,41 @@ menu_history = [] #Stores the order of every menu you've opened
 
 #Functions that load menus
 def set_menu(menu:Menu):
-  global current_menu
-  current_menu = menu
-  current_menu.selected_option = 0 #Resets selection
-  current_menu.display()
+    global current_menu
+    current_menu = menu
+    current_menu.selected_option = 0 #Resets selection
+    current_menu.display()
 
 def switch_menu(menu:Menu):
-  set_menu(menu)
-  menu_history.append(menu)
+    set_menu(menu)
+    menu_history.append(menu)
 
 #Removes current menu and loads previous menu
 def back_menu():
-  menu_history.pop(len(menu_history)-1)
-  set_menu(menu_history[len(menu_history)-1])
+    menu_history.pop(len(menu_history)-1)
+    set_menu(menu_history[len(menu_history)-1])
 
 #Menu objects
 menu_3 = Menu("Menu 3",[
-  MenuOption("Go Back", lambda : back_menu())
+    MenuOption("Go Back", lambda : back_menu())
 ])
 
 menu_2 = Menu("Menu 2", [
-  MenuOption("Menu 3", lambda : switch_menu(menu_3)),
-  MenuOption("Input Test", lambda : input("Input: ")),
-  MenuOption("Go Back", lambda : back_menu())
+    MenuOption("Menu 3", lambda : switch_menu(menu_3)),
+    MenuOption("Input Test", lambda : input("Input: ")),
+    MenuOption("Go Back", lambda : back_menu())
 ])
 #Side menu exists to show that you can get to the same menu from different other menus
 side_menu = Menu("Side Menu", [
-  MenuOption("Menu 2", lambda : switch_menu(menu_2)),
-  MenuOption("Go Back", lambda : back_menu())
+    MenuOption("Menu 2", lambda : switch_menu(menu_2)),
+    MenuOption("Go Back", lambda : back_menu())
 ])
 
 main_menu = Menu("Menu", [
-  MenuOption("Menu 2", lambda : switch_menu(menu_2)),
-  MenuOption("Side Menu", lambda : switch_menu(side_menu)),
-  MenuOption("Unused", None),
-  MenuOption("Exit", quit)
+    MenuOption("Menu 2", lambda : switch_menu(menu_2)),
+    MenuOption("Side Menu", lambda : switch_menu(side_menu)),
+    MenuOption("Unused", None),
+    MenuOption("Exit", quit)
 ])
 
 
@@ -50,11 +50,11 @@ print("(Use arrow keys to navigate)")
 #Main program loop
 running = True
 while running:
-  #Reacts to key input
-  key = readkey()
-  if key == UP:
-    current_menu.up()
-  if key == DOWN:
-    current_menu.down()
-  if key == ENTER:
-    current_menu.confirm()
+    #Reacts to key input
+    key = readkey()
+    if key == UP:
+        current_menu.up()
+    elif key == DOWN:
+        current_menu.down()
+    elif key == ENTER:
+        current_menu.confirm()
--- python_menus/__init__.py
+++ python_menus/__init__.py
@@ -11,51 +11,51 @@ UNDERLINE = "\u0332"
 
 
 class MenuOption:
-  def __init__(self, text:str, func):
-    self.text = text
-    self.func = func
+    def __init__(self, text:str, func):
+        self.text = text
+        self.func = func
 
 
 class Menu:
-  def __init__(self, title:str, options=[]):
-    self.title = title
-    self.options = options
-    self.selected_option = 0
+    def __init__(self, title:str, options=[]):
+        self.title = title
+        self.options = options
+        self.selected_option = 0
 
   #Displays the menu
-  def display(self):
-    os.system('clear')
-    print(Style.RESET_ALL + UNDERLINE.join(self.title))
-    for option in range(len(self.options)):
-      if option == self.selected_option:
-        print(Fore.BLUE + "   > " + self.options[option].text)
-      else:
-        print(Style.RESET_ALL + "     " + self.options[option].text)
+    def display(self):
+        os.system('clear')
+        print(Style.RESET_ALL + UNDERLINE.join(self.title))
+        for option in range(len(self.options)):
+            if option == self.selected_option:
+                print(Fore.BLUE + "   > " + self.options[option].text)
+            else:
+                print(Style.RESET_ALL + "     " + self.options[option].text)
 
-  #Runs option func
-  def confirm(self):
-    if self.options[self.selected_option].func:
-      self.options[self.selected_option].func()
+    #Runs option func
+    def confirm(self):
+        if self.options[self.selected_option].func:
+            self.options[self.selected_option].func()
 
-  #Up and Down selection
-  def up(self):
-    if self.selected_option == 0:
-      self.selected_option = len(self.options)-1
-    else:
-      self.selected_option -= 1
-    self.display()
+    #Up and Down selection
+    def up(self):
+        if self.selected_option == 0:
+            self.selected_option = len(self.options)-1
+        else:
+            self.selected_option -= 1
+        self.display()
 
-  def down(self):
-    if self.selected_option == len(self.options)-1:
-      self.selected_option = 0
-    else:
-      self.selected_option += 1
-    self.display()
+    def down(self):
+        if self.selected_option == len(self.options)-1:
+            self.selected_option = 0
+        else:
+            self.selected_option += 1
+        self.display()
   
-  #Adds or removes MenuOption. Will rarely be used.
-  def add_option(self, text, func):
-    new_option = MenuOption(text, func)
-    self.options.append(new_option)
+    #Adds or removes MenuOption. Will rarely be used.
+    def add_option(self, text, func):
+        new_option = MenuOption(text, func)
+        self.options.append(new_option)
 
-  def remove_option(self, index):
-    self.options.pop(index)
+    def remove_option(self, index):
+        self.options.pop(index)
